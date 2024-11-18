df00b276adf5ea8b243d30b99c65d94aac5e31c5
RodraigoHerrera <herreradiazromero@gmail.com>
2024-11-18 14:23:31 -0400

    Nuevo

---
+++ src/views/index.php
@@ -0,0 +1,37 @@
+<?php include 'navbar.html' ?>
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="../output.css" rel="stylesheet">
+</head>
+<body class="">
+    
+    
+
+    <section class="bg-black flex w-screen h-screen mt-16
+    [&>img]:w-0 [&>img]:flex-grow [&>img]:object-cover [&>img]:opacity-40 [&>img]:transition-all duration-1000 ease-in-out
+    " >
+        <img src="https://images.pexels.com/photos/12115560/pexels-photo-12115560.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+         alt="" class="hover:cursor-crosshair hover:w-[450px] hover:opacity-100">
+        <img src="https://images.pexels.com/photos/18275415/pexels-photo-18275415/free-photo-of-taza-copa-comida-mesa.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+        alt="" class="hover:cursor-crosshair hover:w-[450px] hover:opacity-100">
+        <img src="https://images.pexels.com/photos/19264409/pexels-photo-19264409/free-photo-of-comida-tenedor-bifurcacion-fresco.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+        alt="" class="hover:cursor-crosshair hover:w-[450px] hover:opacity-100">
+        <img src="https://images.pexels.com/photos/25390049/pexels-photo-25390049/free-photo-of-restaurante-comida-carne-patatas-fritas.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+        alt="" class="hover:cursor-crosshair hover:w-[450px] hover:opacity-100">
+        <img src="https://images.pexels.com/photos/7961932/pexels-photo-7961932.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+        alt="" class="hover:cursor-crosshair hover:w-[450px] hover:opacity-100">
+    </section>
+    <section>
+        <div>
+            <h1 class="flex justify-center ">Bienvenido a En tu mesa Cakes & Food 🍰✨</h1>
+            <h1 class="flex justify-center ">¿Listo para vivir una experiencia gastronómica única? En En tu mesa Cakes & Food, combinamos sabores excepcionales con un servicio personalizado para convertir tus momentos especiales en recuerdos inolvidables. Ya sea que estés planeando un evento elegante, un cumpleaños lleno de diversión o simplemente quieras darte un capricho, ¡estamos aquí para ti!</h1>
+        </div>
+    </section>
+    
+
+</body>
+
+
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    './src/**/*.{html,js}', // Incluye todos los archivos HTML y JS dentro de la carpeta src
+    './src/**/*.{html,js,php}', // Incluye todos los archivos HTML y JS dentro de la carpeta src
     './index.html' // Incluye el archivo index.html en la raíz del proyecto si lo tienes ahí
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'roboto': ['Roboto', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
