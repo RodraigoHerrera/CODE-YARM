467586c9842bb96edc3dab9daeb1a4e725447c56
RodraigoHerrera <herreradiazromero@gmail.com>
2025-05-12 18:05:32 -0400

    contra

--- src/model/conexion.php
+++ src/model/conexion.php
@@ -2,7 +2,7 @@
 <?php
 $servername = "localhost";
 $username = "root"; // Usuario por defecto en XAMPP
-$password = "";     // Contraseña por defecto es vacía en XAMPP
+$password = "Assurance1234.";     // Contraseña por defecto es vacía en XAMPP
 $dbname = "sistemaweb"; // Cambia esto al nombre de tu base de datos
 
 // Crear la conexión
